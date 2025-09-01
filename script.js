42ba306f58bff3b95c588ec69cd19939eb150182
varathesquirrel <varathesquirrel@gmail.com>
2025-09-01 16:46:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function copyText(elementId) {
   const text = document.getElementById(elementId).innerText;
   navigator.clipboard.writeText(text).then(() => {
-    alert('Copied to clipboard!');
+
   });
 }
